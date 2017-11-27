c02a9fed69c0a78edd502eeb480195c85f0e35a8
arubino <andrea.rubino@pronetics.it>
2017-11-27 16:38:07 +0100

    aaaasda

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,43 +4,43 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 exports.listAllUsers = function(req, res) {
-  User.find({}).sort({ score: -1 }).limit(10).select({ username: 1, score: 1 }), function(err, task) {
+  User.find({}, function(err, user) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(user);
   });
 };
 
 exports.createUser = function(req, res) {
   var newUser = new User(req.body);
-  newUser.save(function(err, task) {
+  newUser.save(function(err, user) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(user);
   });
 };
 
 exports.readUser = function(req, res) {
-  User.findById(req.params.userId, function(err, task) {
+  User.findById(req.params.userId, function(err, user) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(user);
   });
 };
 
 
 exports.updateUser = function(req, res) {
-  User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, task) {
+  User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, user) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(user);
   });
 };
 
 exports.deleteUser = function(req, res) {
   User.remove({
     _id: req.params.userId
-  }, function(err, task) {
+  }, function(err, user) {
     if (err)
       res.send(err);
     res.json({ message: 'User successfully deleted' });
