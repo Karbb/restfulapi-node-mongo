e48170e5189253d565e5871afb3bfabe9b0966fc
arubino <andrea.rubino@pronetics.it>
2017-11-27 15:18:43 +0100

    added CORS

--- server.js
+++ server.js
@@ -22,6 +22,18 @@ db.once('open', () => {
   console.log('DB connected successfully!');
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  if (req.method === 'Options') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
+    return res.status(200).json({});
+  }
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
